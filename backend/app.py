702b1cc19bfe3bf1c120be5cef7642c6cca156f5
anandcloud24 <thommandruanand54@gmail.com>
2026-01-08 14:25:46 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,9 @@ CORS(app)
 
 # ---------------- DATABASE CONFIG ----------------
 db_config = {
-    "host": "micro.cjs80048kd9y.us-west-2.rds.amazonaws.com",
+    "host": "database-1.c0pyykq2i96e.us-east-1.rds.amazonaws.com",
     "user": "admin",
-    "password": "Cloud123",
+    "password": "Cloud1234",
     "database": "cloud"
 }
 
